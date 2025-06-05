d903ebaef008362a28a15df96d119968689d7101
dhruvdhiman <92265023+ddhruvdhiman@users.noreply.github.com>
2025-06-05 10:49:58 +0530

    mainPageToViewTitleAndBody

---
+++ my-posts-app/src/components/PostCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-4 border rounded-lg shadow hover:bg-gray-50 cursor-pointer transition">
+    <h2 class="text-xl font-semibold">{{ post.title }}</h2>
+    <p class="text-gray-600 mt-2">{{ post.body }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(['post'])
+</script>
