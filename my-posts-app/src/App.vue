be83be40e864c4a800fdc31f1ffdfe6d9496733f
dhruvdhiman <92265023+ddhruvdhiman@users.noreply.github.com>
2025-06-05 10:49:18 +0530

    commit

--- my-posts-app/src/App.vue
+++ my-posts-app/src/App.vue
@@ -1,21 +1,13 @@
 <script setup>
+import { RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import PostCard from './components/PostCard.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <RouterView />
 </template>
+
 
 <style scoped>
 header {
